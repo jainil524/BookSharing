39165bafbc3accbc194d61ce59f2517eb9418818
kishan93273110 <techno931644@gmail.com>
2022-02-16 19:20:58 +0530

    upload profile page

--- php/profile.php
+++ php/profile.php
@@ -3,47 +3,37 @@ session_start();
 require "dbconfig.php";
 $uimg = "";
 $dprofile = $_FILES["dp"];
-// echo print_r($dprofile);
-// exit();
 $na = $_POST['fname'];
 $una = $_POST['username'];
 $adr = $_POST['address'];
 $em = $_POST['email'];
 $pinc = $_POST['pincode'];
-
-if(empty($na) || empty($una) || empty($adr) || empty($na)){
+$fileUpload = "";
+if (empty($na) || empty($una) || empty($adr) || empty($na)) {
     echo "Please fill all fields";
-}    
-
-if(isset($_FILES['dp'])){ 
-    if(move_uploaded_file($_FILES['dp']['tmp_name'], "media/profile_photos/".$_FILES['dp']['name'])){
-        $uimg = "media/profile_photos/".$_FILES['dp']['name'];  
-        echo "yes";
-        exit();
-    }
-} 
-else{
-    $uimg = "media/profile_photos/default.svg";
-    exit();
 }
-    $UpdateQuery = "UPDATE user 
-                    SET fname = '".$na."',
-                    email= '".$em."',
-                    user_name= '".$una."',
-                    Profile_photo= '".$uimg."',
-                    address = '".$adr."',
-                    pincode=  '".$pinc."'
+if (!empty($_FILES['dp']["name"])) {
+    if (move_uploaded_file($_FILES['dp']['tmp_name'], "../media/profile_photos/" . $_FILES['dp']['name'])) {
+        $uimg = "media/profile_photos/" . $_FILES['dp']['name'];
+        $fileUpload = " Profile_photo= '" . $uimg . "',";
+    }
+} else {
+    $fileUpload = "";
+}
+$UpdateQuery = "UPDATE user 
+                    SET fname = '" . $na . "',
+                    email= '" . $em . "',
+                    user_name= '" . $una . "',
+                   " . $fileUpload . "
+                    address = '" . $adr . "',
+                    pincode=  '" . $pinc . "'
                     WHERE user_name ='" . $_SESSION['username'] . "' ";
 
-    mysqli_query($con, $UpdateQuery);
-        if($UpdateQuery){
-            $_SESSION['username'] = $una;
-            $_SESSION['profile_photo'] = $uimg;
-            echo "successfully";
-        }
-        else{
-            echo mysqli_error($conn);
-        }
-
-
-?>
+mysqli_query($con, $UpdateQuery);
+if ($UpdateQuery) {
+    $_SESSION['username'] = $una;
+    $_SESSION['profile_photo'] = $uimg;
+    echo "successfully";
+} else {
+    echo mysqli_error($conn);
+}
