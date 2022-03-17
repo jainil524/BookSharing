37dd81aa30429c1db38dbcfb7ac2cc6aad0b8c1e
kishan93273110 <techno931644@gmail.com>
2022-03-17 18:16:36 +0530

    chages made by kishan

--- view.php
+++ view.php
@@ -14,51 +14,62 @@ if (empty($book)) {
     exit;
 }
 ?>
-<div class="main-con">
-    <div class="back-btn">
-        <a href="index.php"><img src="./img/back.png" alt=""></a>
-    </div>
-
-    <div class="book-container">
-        <div class="img-con">
-            <img src="<?php echo (file_exists($book["book_coverpage"]) == false ? 'img/logos.png' : $book["book_coverpage"]); ?>" alt="book_img">
+<div>
+    <div class="main-con">
+        <div class="back-btn">
+            <a href="index.php"><img src="./img/back.png" alt=""></a>
         </div>
-        <div class="info-container">
-            <h2><?php echo $book["book_name"]; ?></h2>
-            <div class="author">
-                <p><?php echo $book["book_description"]; ?></p>
-            </div>
-            <div class="author">
-                <h5>Author:</h5>
-                <h3><?php echo $book["book_author"]; ?></h3>
-            </div>
 
-            <div class="publisher">
-                <h5>Publisher year:</h5>
-                <h3><?php echo $book["book_publish_year"]; ?></h3>
+        <div class="book-container">
+            <div class="img-con">
+                <img src="<?php echo (file_exists($book["book_coverpage"]) == false ? 'img/logos.png' : $book["book_coverpage"]); ?>" alt="book_img">
             </div>
-            <div class="author">
-                <h5>Price:</h5>
-                <h3>&#8377; <?php echo $book["book_price"]; ?></h3>
-            </div>
-            <div class="btn-container">
-                <button onclick="buy()">Buy</button>
-            </div>
-            <div class="seller-detail">
-                <h5>Seller:</h5>
-                <h3><?php echo $book["user_name"]; ?>
-                    <div class="chat-icon"><a href="chat.php?id=<?php echo $book["user_id"]; ?> "><img src="img/chat.png" alt=""></a></div>
-                    <div class="report-icon"><img src="img/report.png" alt=""></div>
-                </h3>
-            </div>
-            <div class="error">
-                <h5></h5>
+            <div class="info-container">
+                <h2><?php echo $book["book_name"]; ?></h2>
+                <div class="author">
+                    <p><?php echo $book["book_description"]; ?></p>
+                </div>
+                <div class="author">
+                    <h5>Author:</h5>
+                    <h3><?php echo $book["book_author"]; ?></h3>
+                </div>
+
+                <div class="publisher">
+                    <h5>Publisher year:</h5>
+                    <h3><?php echo $book["book_publish_year"]; ?></h3>
+                </div>
+                <div class="author">
+                    <h5>Price:</h5>
+                    <h3>&#8377; <?php echo $book["book_price"]; ?></h3>
+                </div>
+                <div class="btn-container">
+                    <button onclick="buy()">Buy</button>
+                </div>
+                <div class="seller-detail">
+                    <h5>Seller:</h5>
+                    <h3><?php echo $book["user_name"]; ?>
+                        <div class="chat-icon"><a href="chat.php?id=<?php echo $book["user_id"]; ?> "><img src="img/chat.png" alt=""></a></div>
+                        <div class="report-icon"><img src="img/report.png" alt=""></div>
+                    </h3>
+                </div>
+                <div class="error">
+                    <h5></h5>
+                </div>
             </div>
         </div>
     </div>
-
     <div class="report-pop-up">
-
+        <div class="pop-up">
+            <div class="heading">
+                Report to <span><?php echo $book["user_name"]; ?></span>
+            </div>
+            <!--https://cdn.optinmonster.com/wp-content/uploads/2018/02/wholewhale-website-modal-popup.jpg-->
+            <div class="reason-con">
+                <select name="reason" id="reason">
+                    <option value="some">some</option>
+                </select>
+            </div>
+        </div>
     </div>
 </div>
 <script src="js/AJAX.js"></script>
