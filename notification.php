13d901a056b8dcce80d4e15d83ee6097fb3a1361
jainil524 <jainilprajapati8095@gmail.com>
2022-03-22 21:58:17 +0530

    made changes in notification ui

--- notification.php
+++ notification.php
@@ -1,43 +1,49 @@
 <?php
-    $title="Notification - BookSharing";
-    $css_file_name = "notification";
-    require "php/navbar.php";
-    require "php/RoleChecker.php";
-    Rlchecker("user",403,"Access Denied","You don't have access to this page");
-    Rlchecker("DeliveryGuy",403,"Access Denied","You don't have access to this page");
+$title = "Notification - BookSharing";
+$css_file_name = "notification";
+
+require "php/navbar.php";
+require "php/LoginCheck.php";
+require "php/RoleChecker.php";
+Rlchecker("user", 403, "Access Denied", "You don't have access to this page");
+Rlchecker("DeliveryGuy", 403, "Access Denied", "You don't have access to this page");
 ?>
 
 <div id="container">
     <div id="notifications">
-        <details>
-            <summary>Warnings</summary>
-            <ul>
-                <li>warn 1</li>
-                <li>warn 2</li>
-                <li>warn 3</li>
-            </ul>
-        </details>
 
-        <details>
-            <summary>Reports</summary>
-            <ul>
-                <li>Reports 1</li>
-                <li>Reports 2</li>
-                <li>Reports 3</li>
-            </ul>
-        </details>
-        
-        <details>
-            <summary>Book OTP</summary>
-            <ul>
-                <li>Book OTP 1</li>
-                <li>Book OTP 2</li>
-                <li>Book OTP 3</li>
-            </ul>
-        </details>
+        <!-- OTP container -->
+        <div class="BookPin subcontainer">
+            <div>
+            </div>
+            <div class="card">
+                <div class="card-img">
+                    <img src="img/BookPin_icon.png" alt="">
+                </div>
+                <div class="card-details">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae cupiditate voluptatem atque sequi laborum, nisi fugit magnam! Facere iure sed sequi possimus error voluptatibus quisquam? Aliquid nisi accusantium aperiam voluptatibus, sequi perferendis dolore quis, aut veritatis repellat doloremque non delectus.
+                </div>
+                <div class="watermark">Book Pin</div>
+
+            </div>
+        </div>
+
+        <!-- Warning msgs container -->
+        <div class="Warnings subcontainer">
+            <div class="card">
+                <div class="card-img">
+                    <img src="img/warning_icon.svg" alt="">
+                </div>
+                <div class="card-details">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, laborum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae cupiditate voluptatem atque sequi laborum, nisi fugit magnam! Facere iure sed sequi possimus error voluptatibus quisquam? Aliquid nisi accusantium aperiam voluptatibus, sequi perferendis dolore quis, aut veritatis repellat doloremque non delectus.
+                </div>
+                <div class="watermark">Warnings</div>
+            </div>
+        </div>
+
     </div>
 </div>
 
 <?php
-    require "php/footer.php";
+require "php/footer.php";
 ?>
