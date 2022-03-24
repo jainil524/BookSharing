6e340df12071e7f1dc73af5a0b40250c1815f4f8
jainil524 <jainilprajapati8095@gmail.com>
2022-03-24 12:38:09 +0530

    some changes

--- php/navbar.php
+++ php/navbar.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    link:
+    <link rel="shortcut icon" href="img/logo_without_text.png" type="image/x-icon">
     <script defer src="js/AJAX.js"></script>
     <link rel="stylesheet" href="css/<?php echo $css_file_name ?>.css">
     <link rel="stylesheet" href="css/common.css">
