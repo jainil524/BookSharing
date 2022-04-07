ea34bf18af2ac06766f37c327b49274531b551b9
jainil524 <jainilprajapati8095@gmail.com>
2022-04-07 09:52:21 +0530

    book_transaction bug is where buying book shows the error

--- php/book_transaction.php
+++ php/book_transaction.php
@@ -3,7 +3,8 @@ require "dbconfig.php";
 session_start();
 $rand = rand(100000,999999);
 
-$SelectDeliveryItemQuery = "SELECT dg.delivery_guy_id as dgID,(SELECT COUNT(bt.delivery_guy_id) as dd FROM book_transaction bt WHERE (bt.delivery_guy_id = dg.delivery_guy_id AND bt.delivery_guy_id IS NOT NULL GROUP BY bt.delivery_guy_id) AND IsDelivered = 0) as mincount FROM delivery_guy dg ORDER BY mincount LIMIT 1";
+$SelectDeliveryItemQuery = "SELECT dg.delivery_guy_id AS dgID, ( SELECT COUNT(bt.delivery_guy_id) AS dd FROM book_transaction bt WHERE ( bt.delivery_guy_id = dg.delivery_guy_id AND bt.delivery_guy_id IS NOT NULL ) AND IsDelivered = 0 GROUP BY bt.delivery_guy_id) AS mincount FROM delivery_guy dg ORDER BY mincount LIMIT 1";
+                            
 $SelectDeliveryItemFire = mysqli_query($con,$SelectDeliveryItemQuery);
 $row = mysqli_fetch_assoc($SelectDeliveryItemFire);
 
